4f06bfccfc08a41aa80bfc4fdaa81c76e211f328
lavalamp <cegrayson3@gmail.com>
2022-11-29 13:35:26 -0800

    Update Terraform to have Cloudflare specifications.

--- cors_service/terraform/main.tf
+++ cors_service/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "heroku/heroku"
       version = "~> 5.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -11,9 +15,22 @@ variable "heroku_app_name" {
   description = "Name to assign to the Heroku app"
 }
 
+variable "cloudflare_api_token" {
+  description = "API token to use for communication with Cloudflare"
+}
+
+variable "host_domains" {
+  type = list(string)
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
 resource "heroku_app" "cors_hunter" {
   name = var.heroku_app_name
   region = "us"
+  acm = false
 
   config_vars = {
     "PROD" = "1"
@@ -39,6 +56,26 @@ resource "heroku_formation" "cors_hunter" {
   quantity = 1
   size = "hobby"
   depends_on = [heroku_build.cors_hunter]
+}
+
+resource "heroku_domain" "this" {
+  for_each = toset(var.host_domains)
+  app_id = heroku_app.cors_hunter.id
+  hostname = each.key
+}
+
+resource "cloudflare_zone" "this" {
+  for_each = toset(var.host_domains)
+  zone = each.key
+}
+
+resource "cloudflare_record" "this" {
+  for_each = zipmap([for zone in cloudflare_zone.this: zone.id], [for domain in heroku_domain.this: domain.cname])
+  zone_id = each.key
+  name = "*"
+  value = each.value
+  type = "CNAME"
+  proxied = false
 }
 
 output "cors_hunter_url" {
